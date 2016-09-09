327920e0dff98a77ef83f3d8c036c7f513e99506
James Turello <jturello@yahoo.com>
2016-09-09 16:23:07 -0700

    delete region specs

--- spec/models/region_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe Region do
-  it { should validate_presence_of :name }
-  it { should validate_presence_of :blurb }
-  it { should validate_presence_of :description }
-
-  it { should have_many :countries}
-end
