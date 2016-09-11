10e2d74592e844f7260588f87bf096fed72adfe1
James Turello <jturello@yahoo.com>
2016-09-11 12:41:01 -0700

    add countries new/create controllers/view

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -7,4 +7,24 @@ class CountriesController < ApplicationController
   def show
     @country = Country.find(params[:id])
   end
+
+  def new
+    @country = Country.new
+  end
+
+  def create
+    @country = Country.new(country_params)
+    if @country.save
+      flash[:notice] = "Country successfully added!"
+      redirect_to countries_path
+    else
+      flash[:alert] = "Country not added. Try again!"
+      render :new
+    end
+  end
+
+  private
+  def country_params
+    params.require(:country).permit(:name, :blurb, :description, :language, :image_url)
+  end
 end
