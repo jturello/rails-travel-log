a7d2e83b99bc3988f217a336b18a08f9963e6e80
James Turello <jturello@yahoo.com>
2016-09-11 16:51:34 -0700

    add destination controllers & views

---
+++ app/controllers/destinations_controller.rb
@@ -0,0 +1,52 @@
+class DestinationsController < ApplicationController
+
+  def show
+    @destination = Destination.find(params[:id])
+  end
+
+  def new
+    @country = Country.find(params[:country_id])
+    @destination = @country.destinations.new
+  end
+
+  def create
+    @country = Country.find(params[:country_id])
+    @destination = @country.destinations.new(destination_params)
+    if @destination.save
+      flash[:notice] = "Destination successfully added!"
+      redirect_to country_path(@destination.country)
+    else
+      flash[:alert] = "Destination not added. Try again!"
+      render :new
+    end
+  end
+
+  def edit
+    @destination = Destination.find(params[:id])
+    @country = Country.find(@destination.country)
+  end
+
+  def update
+    # @country = Country.find(params[:country_id])
+    @destination = Destination.find(params[:id])
+    # @destination = Destinations.find(params[:id])
+    if @destination.update(destination_params)
+      flash[:notice] = "Destination updated successfully!"
+      redirect_to country_path(@destination.country)
+    else
+      flash[:alert] = "Destination not saved. Try again!"
+      render :edit
+    end
+  end
+
+  def destroy
+    @destination = Destination.find(params[:id])
+    @destination.destroy
+    redirect_to country_path(@destination.country)
+  end
+
+  private
+  def destination_params
+    params.require(:destination).permit(:name, :blurb, :description, :image_url)
+  end
+end
