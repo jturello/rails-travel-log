a06b75dcd07dcf16bbdeb22f367809e1372572e1
James Turello <jturello@yahoo.com>
2016-09-12 04:34:11 -0700

    refactor new_country_path specs

--- spec/features/new_country_pages_spec.rb
+++ spec/features/new_country_pages_spec.rb
@@ -1,44 +1,56 @@
 require('rails_helper')
 
 describe "new_country_path" do
-  it "adds a new country" do
-    visit root_path
-    click_on 'New Country'
-    fill_in 'Name', :with => 'Modor'
-    fill_in 'Blurb', :with => 'Sights to see - the Eye of Sauron'
-    fill_in 'Description', :with => 'Billowing clouds of choking soot'
-    click_on 'Create Country'
-    expect(page).to have_content('Modor')
+  context "with all required input provided" do
+    it "adds a new country" do
+      visit root_path
+      click_on 'New Country'
+      fill_in 'Name', :with => 'Modor'
+      fill_in 'Blurb', :with => 'Sights to see - the Eye of Sauron'
+      fill_in 'Description', :with => 'Billowing clouds of choking soot'
+      click_on 'Create Country'
+      expect(page).to have_content('Modor')
+    end
   end
+  context "when all required input not provided" do
+    it "displays a warning message when Name is blank" do
+      visit root_path
+      click_on 'New Country'
 
-  it "displays a warning message when Name is blank" do
-    visit root_path
-    click_on 'New Country'
-    fill_in 'Name', :with => 'Modor'
+      fill_in 'Blurb', :with => 'Sights to see - the Eye of Sauron'
+      fill_in 'Description', :with => 'Billowing clouds of choking soot'
+      click_on 'Create Country'
+      expect(page).to have_content('Country not added. Try again!')
+    end
 
-    fill_in 'Description', :with => 'Billowing clouds of choking soot'
-    click_on 'Create Country'
-    expect(page).to have_content('Country not added. Try again!')
+    it "displays a warning message when Blurb is blank" do
+      visit root_path
+      click_on 'New Country'
+      fill_in 'Name', :with => 'Modor'
+
+      fill_in 'Description', :with => 'Billowing clouds of choking soot'
+      click_on 'Create Country'
+      expect(page).to have_content('Country not added. Try again!')
+    end
+
+    it "displays a warning message when Description is blank" do
+      visit root_path
+      click_on 'New Country'
+      fill_in 'Name', :with => 'Modor'
+      fill_in 'Blurb', :with => 'Sights to see - the Eye of Sauron'
+
+      click_on 'Create Country'
+      expect(page).to have_content('Country not added. Try again!')
+    end
+
+    it "redisplays the New Country page" do
+      visit root_path
+      click_on 'New Country'
+
+      fill_in 'Blurb', :with => 'Sights to see - the Eye of Sauron'
+      fill_in 'Description', :with => 'Billowing clouds of choking soot'
+      click_on 'Create Country'
+      expect(page).to have_css("h1", :text => "New Country")
+    end
   end
-
-  it "displays a warning message when Blurb is blank" do
-    visit root_path
-    click_on 'New Country'
-    fill_in 'Name', :with => 'Modor'
-    fill_in 'Blurb', :with => 'Sights to see - the Eye of Sauron'
-
-    click_on 'Create Country'
-    expect(page).to have_content('Country not added. Try again!')
-  end
-
-  it "displays a warning message when Name is blank" do
-    visit root_path
-    click_on 'New Country'
-
-    fill_in 'Blurb', :with => 'Sights to see - the Eye of Sauron'
-    fill_in 'Description', :with => 'Billowing clouds of choking soot'
-    click_on 'Create Country'
-    expect(page).to have_content('Country not added. Try again!')
-  end
-
 end
