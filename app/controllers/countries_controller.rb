341d75a203ac699746d0e486668bcb93746eeab2
James Turello <jturello@yahoo.com>
2016-09-16 12:38:24 -0700

    hide add/update/delete when user not signed in

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,5 +1,6 @@
 class CountriesController < ApplicationController
-
+  before_filter :authenticate_user!, :except => [:index, :show]
+  
   def index
     @countries = Country.all
   end
--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,5 +1,6 @@
 class DestinationsController < ApplicationController
-
+  before_filter :authenticate_user!, :except => [:index, :show]
+  
   def show
     @destination = Destination.find(params[:id])
   end
