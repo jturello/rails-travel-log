dfd475d5070df0be5cbaadeb72f2284898d95556
James Turello <jturello@yahoo.com>
2016-09-16 03:28:53 -0700

    move specs to files with corrected path names

--- spec/features/country_destination_pages_spec.rb
+++ spec/features/country_destination_pages_spec.rb
@@ -1,18 +1,6 @@
 require 'rails_helper'
 
-describe 'new_country_destination_path' do
-
-  it 'creates a new destination' do
-    @country = FactoryGirl.create :country
-    visit root_path
-    click_on 'Mordor : Sights to see - the Eye of Sauron!'
-    click_on 'Add Destination'
-    fill_in 'Name', :with => 'Barad Dur'
-    fill_in 'Blurb', :with => 'Beautiful city of Sauron with postcard perfect views of Mount Doom'
-    fill_in 'Description', :with => "At the foothills of Ered Lithiu and just a 2 day's march to Mount Doom!"
-    click_on 'Create Destination'
-    expect(page).to have_content 'Barad Dur'
-  end
+describe 'country_destination_path' do
 
   context 'when country link is clicked' do
     it 'displays the country detail page' do
