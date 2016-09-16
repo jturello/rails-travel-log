430fef447f84f3b5575de9e4cb27baec44bdbfa7
James Turello <jturello@yahoo.com>
2016-09-16 05:42:07 -0700

    refactor specs

--- spec/features/country_destination_pages_spec.rb
+++ spec/features/country_destination_pages_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'country_destination_path' do
 
   context 'when country link is clicked' do
-    it 'displays the country detail page' do
+    it 're-displays the country detail page' do
       @destination = FactoryGirl.create :destination
       visit country_destination_path @destination.country, @destination
       click_on @destination.country.name
@@ -14,17 +14,22 @@ describe 'country_destination_path' do
   context 'when Delete is clicked' do
     it "deleted destionation's country page displays" do
       @destination = FactoryGirl.create :destination
-
       visit country_destination_path @destination.country, @destination
       click_on 'Delete'
       expect(page).to have_content "Mordor's Country Detail Page"
     end
+
+    it "displays flash notice - Destination deleted successfully!" do
+      @destination = FactoryGirl.create :destination
+      visit country_destination_path @destination.country, @destination
+      click_on 'Delete'
+      expect(page).to have_content "Destination deleted successfully!"
+    end
   end
 
   context 'when Edit is clicked' do
-    it "Update Destionation page displays" do
+    it "displays Update Destionation page" do
       @destination = FactoryGirl.create :destination
-
       visit country_destination_path @destination.country.id, @destination.id
       click_on 'Edit'
       expect(page).to have_content "Update Destination"
--- spec/features/new_country_destination_pages_spec.rb
+++ spec/features/new_country_destination_pages_spec.rb
@@ -2,16 +2,77 @@ require 'rails_helper'
 
 describe 'new_country_destination_path' do
 
-  it 'creates a new destination' do
-    @country = FactoryGirl.create :country
-    visit root_path
-    click_on 'Mordor : Sights to see - the Eye of Sauron!'
-    click_on 'Add Destination'
-    fill_in 'Name', :with => 'Barad Dur'
-    fill_in 'Blurb', :with => 'Beautiful city of Sauron with postcard perfect views of Mount Doom'
-    fill_in 'Description', :with => "At the foothills of Ered Lithiu and just a 2 day's march to Mount Doom!"
-    click_on 'Create Destination'
-    expect(page).to have_content 'Barad Dur'
+  context 'when add succeeds' do
+    it 'creates a new destination' do
+      @country = FactoryGirl.create :country
+      visit root_path
+      click_on 'Mordor : Sights to see - the Eye of Sauron!'
+      click_on 'Add Destination'
+      fill_in 'Name', :with => 'Barad Dur'
+      fill_in 'Blurb', :with => 'Beautiful city of Sauron with postcard perfect views of Mount Doom'
+      fill_in 'Description', :with => "At the foothills of Ered Lithiu and just a 2 day's march to Mount Doom!"
+      click_on 'Create Destination'
+      expect(page).to have_content 'Barad Dur'
+    end
+
+    it 're-displays Country Detail Page' do
+      @country = FactoryGirl.create :country
+      visit country_path @country
+      click_on 'Add Destination'
+      fill_in 'Name', :with => 'Barad Dur'
+      fill_in 'Blurb', :with => 'Beautiful city of Sauron with postcard perfect views of Mount Doom'
+      fill_in 'Description', :with => "At the foothills of Ered Lithiu and just a 2 day's march to Mount Doom!"
+      click_on 'Create Destination'
+      expect(page).to have_content "Mordor's Country Detail Page"
+    end
+
+    it 'displays added destionation on Country Detail Page' do
+      @country = FactoryGirl.create :country
+      visit country_path @country
+      click_on 'Add Destination'
+      fill_in 'Name', :with => 'Barad Dur'
+      fill_in 'Blurb', :with => 'Beautiful city of Sauron with postcard perfect views of Mount Doom'
+      fill_in 'Description', :with => "At the foothills of Ered Lithiu and just a 2 day's march to Mount Doom!"
+      click_on 'Create Destination'
+      expect(page).to have_content 'Barad Dur : Beautiful city of Sauron with postcard perfect views of Mount Doom'
+    end
+
+    it 'displays flash notice - Destination successfully added!' do
+      @country = FactoryGirl.create :country
+      visit country_path @country
+      click_on 'Add Destination'
+      fill_in 'Name', :with => 'Barad Dur'
+      fill_in 'Blurb', :with => 'Beautiful city of Sauron with postcard perfect views of Mount Doom'
+      fill_in 'Description', :with => "At the foothills of Ered Lithiu and just a 2 day's march to Mount Doom!"
+      click_on 'Create Destination'
+      expect(page).to have_content 'Destination successfully added!'
+    end
   end
 
+  context 'when add fails' do
+    it 're-displays New Destination Page' do
+      @country = FactoryGirl.create :country
+      visit new_country_destination_path @country
+      # click_on 'Add Destination'
+      click_on 'Create Destination'
+      expect(page).to have_content 'New Destination'
+    end
+
+    it 'displays flash alert - Destination not added. Try again!' do
+      @country = FactoryGirl.create :country
+      visit country_path @country
+      click_on 'Add Destination'
+      click_on 'Create Destination'
+      expect(page).to have_content 'Destination not added. Try again!'
+    end
+
+    it 'does not add new destination' do
+      @country = FactoryGirl.create :country
+      visit country_path @country
+      click_on 'Add Destination'
+      click_on 'Create Destination'
+      click_on @country.name
+      expect(page).to have_content 'No Destinations added yet!'
+    end
+  end
 end
