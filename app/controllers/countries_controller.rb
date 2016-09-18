caee33e6f73b9b508aba6d6aa418de04980d4ffd
James Turello <jturello@yahoo.com>
2016-09-18 14:28:03 -0700

    add user model validation tests

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,5 +1,5 @@
 class CountriesController < ApplicationController
-  before_filter :authenticate_user!, :except => [:index, :show]
+  before_action :authenticate_user!, :except => [:index, :show]
   
   def index
     @countries = Country.all
