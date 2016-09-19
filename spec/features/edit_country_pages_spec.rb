d8b4bc34387af8ab06d2796224c38c99b61edf7f
James Turello <jturello@yahoo.com>
2016-09-18 18:53:06 -0700

    refactor edit country specs

--- spec/features/edit_country_pages_spec.rb
+++ spec/features/edit_country_pages_spec.rb
@@ -7,46 +7,52 @@ describe "edit_country_path" do
     login_as(user, :scope => :user)
   end
 
-  it "changes Name when Name is updated" do
-    @country = FactoryGirl.create(:country)
-    visit root_path
-    click_on 'Mordor : Sights to see - the Eye of Sauron!'
-    click_on 'Edit'
-    fill_in 'Name', :with => 'Moria'
-    fill_in 'Blurb', :with => 'Sights to see - the Crack of Doom'
-    fill_in 'Description', :with => 'Wet, damp, and dark. What better place to spend your vacation?'
-    click_on 'Update Country'
-    expect(page).to have_content('Moria')
+  context 'when Name is updated' do
+
+    it "updates Name" do
+      @country = FactoryGirl.create(:country)
+      visit root_path
+      click_on 'Mordor : Sights to see - the Eye of Sauron!'
+      click_on 'Edit'
+      fill_in 'Name', :with => 'Moria'
+      fill_in 'Blurb', :with => 'Sights to see - the Crack of Doom'
+      fill_in 'Description', :with => 'Wet, damp, and dark. What better place to spend your vacation?'
+      click_on 'Update Country'
+      expect(page).to have_content('Moria')
+    end
+
+    it "does not change Blurb when only Name is updated" do
+      @country = FactoryGirl.create :country
+      visit root_path
+      click_on 'Mordor : Sights to see - the Eye of Sauron!'
+      click_on 'Edit'
+      fill_in 'Name', :with => 'Moria'
+      click_on 'Update Country'
+      expect(page).to have_content('Sights to see - the Eye of Sauron!')
+    end
   end
 
-  it "does not change Blurb when only Name is updated" do
-    @country = FactoryGirl.create :country
-    visit root_path
-    click_on 'Mordor : Sights to see - the Eye of Sauron!'
-    click_on 'Edit'
-    fill_in 'Name', :with => 'Moria'
-    click_on 'Update Country'
-    expect(page).to have_content('Sights to see - the Eye of Sauron!')
+  context 'when Cancel Update is clicked' do
+    it "does not update country" do
+      @country = FactoryGirl.create(:country)
+      visit root_path
+      click_on 'Mordor : Sights to see - the Eye of Sauron!'
+      click_on 'Edit'
+      fill_in 'Name', :with => 'Moria'
+      click_on 'Cancel Update'
+      expect(page).not_to have_content("Moria's Country Detail Page")
+    end
   end
 
-  it "does not update country when Cancel Update is clicked" do
-    @country = FactoryGirl.create(:country)
-    visit root_path
-    click_on 'Mordor : Sights to see - the Eye of Sauron!'
-    click_on 'Edit'
-    fill_in 'Name', :with => 'Moria'
-    click_on 'Cancel Update'
-    expect(page).not_to have_content("Moria's Country Detail Page")
+    context 'when update succeeds' do
+    it "displays a flash notice" do
+      @country = FactoryGirl.create(:country)
+      visit root_path
+      click_on 'Mordor : Sights to see - the Eye of Sauron!'
+      click_on 'Edit'
+      fill_in 'Name', :with => 'Moria'
+      click_on 'Update Country'
+      expect(page).to have_content('Country updated successfully!')
+    end
   end
-
-  it "displays a flash notice when update succeeds" do
-    @country = FactoryGirl.create(:country)
-    visit root_path
-    click_on 'Mordor : Sights to see - the Eye of Sauron!'
-    click_on 'Edit'
-    fill_in 'Name', :with => 'Moria'
-    click_on 'Update Country'
-    expect(page).to have_content('Country updated successfully!')
-  end
-
 end
