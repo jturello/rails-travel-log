9000553da2b8c30796995c7ceea808885987a933
James Turello <jturello@yahoo.com>
2016-09-23 11:32:12 -0700

    add post routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: :account, path_names: {sign_in: 'login', sign_out: 'logout'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'countries#index'
 
+  resources :users do
+    resources :posts
+  end
   resources :countries do
-    resources :destinations, :except => [:index]
+    resources :posts
+    resources :destinations, :except => [:index] do
+      resources :posts
+    end
   end
 end
