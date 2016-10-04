d328976b06d4b3cae7958420f71a9dc2a6428a32
James Turello <jturello@yahoo.com>
2016-10-04 15:16:28 -0700

    limit post routes to new/create/destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users, path: :account, path_names: {sign_in: 'login', sign_out: 'logout'}
-  
+
   root :to => 'countries#index'
 
   resources :users do
     resources :posts
   end
   resources :countries do
-    resources :posts
+    resources :posts, :only => [:new, :create, :destroy]
     resources :destinations, :except => [:index] do
-      resources :posts
+      resources :posts, :only => [:new, :create, :destroy]
+      resources :forecasts, :only => [:new, :create]
     end
   end
 end
