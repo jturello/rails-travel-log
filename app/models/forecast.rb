fe8ddd2bd20f944cdfdb309a4a40083e8f9c4b08
James Turello <jturello@yahoo.com>
2016-10-04 16:51:21 -0700

    add api call method to forecast model

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,5 +1,15 @@
 class Forecast < ActiveRecord::Base
   belongs_to :destination
+  before_create :get_forecast
+
 
   validates :api_timestamp, :destination_id, presence: true
+
+  private
+  def get_forecast
+    response = RestClient::Request.new(
+      :method => :get,
+      :url => 'https://api.darksky.net/forecast/ENV[DARKSKY_API_KEY]/37.8267,-122.4233'
+      ).execute
+  end
 end
