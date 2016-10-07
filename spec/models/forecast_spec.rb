dd4dd24540c45d3957719e800c63654a34ea05c6
James Turello <jturello@yahoo.com>
2016-10-07 02:15:55 -0700

    add presence validation to forecasts lat/lng

--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 describe Forecast do
 
   it { should validate_presence_of :destination_id }
-  it { should validate_presence_of :api_timestamp }
+  it { should validate_presence_of :latitude }
+  it { should validate_presence_of :longitude }
   it { should belong_to :destination }
   it { should have_db_index :destination_id }
 
