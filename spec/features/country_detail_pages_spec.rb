220f04de25263f4bbcc812aaf6c2fff08de1902a
James Turello <jturello@yahoo.com>
2017-01-18 03:20:07 -0800

    add tests for Add Posts link on country detail page

--- spec/features/country_detail_pages_spec.rb
+++ spec/features/country_detail_pages_spec.rb
@@ -7,71 +7,88 @@ describe 'country_path' do
     login_as(user, :scope => :user)
   end
 
-  context 'when Add Destination link is clicked' do
-    it "displays Add Destination page" do
-      @country = FactoryGirl.create(:country)
-      visit country_path @country
-      click_on 'Add Destination'
-      expect(page).to have_css('h2', :text => 'New Destination')
-    end
-  end
+  context 'when user is logged in' do
 
-    context 'when Destination link is clicked' do
-      it 'displays destination detail page' do
-        @destination = FactoryGirl.create :destination
-        visit country_path @destination.country
-        expect(page).to have_content 'Barad Dur'
+    it "does displays the 'Add Post' link" do
+      @country = FactoryGirl.create :country
+      visit country_path(@country)
+      expect(page).to have_link('Add Post', :href=>new_country_post_path(@country))
+    end
+
+    context 'when Add Destination link is clicked' do
+      it "displays Add Destination page" do
+        @country = FactoryGirl.create(:country)
+        visit country_path @country
+        click_on 'Add Destination'
+        expect(page).to have_css('h2', :text => 'New Destination')
       end
     end
 
-  context 'when Delete is clicked' do
-    it "deletes country" do
-      @country = FactoryGirl.create(:country)
-      visit country_path @country
-      click_on 'Delete'
-      expect(page).not_to have_content('Mordor')
-    end
-  end
+      context 'when Destination link is clicked' do
+        it 'displays destination detail page' do
+          @destination = FactoryGirl.create :destination
+          visit country_path @destination.country
+          expect(page).to have_content 'Barad Dur'
+        end
+      end
 
-  context 'when Edit link is clicked' do
-    it 'displays Edit Country page' do
-      @country = FactoryGirl.create :country
-      visit country_path @country
-      click_on 'Edit'
-      expect(page).to have_css("h1", :text => 'Update Country')
+    context 'when Delete is clicked' do
+      it "deletes country" do
+        @country = FactoryGirl.create(:country)
+        visit country_path @country
+        click_on 'Delete'
+        expect(page).not_to have_content('Mordor')
+      end
+    end
+
+    context 'when Edit link is clicked' do
+      it 'displays Edit Country page' do
+        @country = FactoryGirl.create :country
+        visit country_path @country
+        click_on 'Edit'
+        expect(page).to have_css("h1", :text => 'Update Country')
+      end
     end
   end
 
   context 'when user is not logged in' do
-    it "does not display the 'Edit' link" do
+
+    before :each do
       logout :user
+    end
+
+    it "does not display the 'Edit' link" do
       @country = FactoryGirl.create :country
-      visit edit_country_path(@country)
+      visit country_path(@country)
       expect(page).to_not have_link('Edit', :href=>edit_country_path(@country))
     end
 
     it "does not display the 'Delete' link" do
-      logout :user
       @country = FactoryGirl.create :country
-      visit edit_country_path(@country)
+      visit country_path(@country)
       expect(page).to_not have_link('Delete', :href=>country_path(@country))
     end
 
     it "does not display the 'Add Destination' link" do
-      logout :user
       @country = FactoryGirl.create :country
-      visit edit_country_path(@country)
-      expect(page).to_not have_link('Add Destination', :href=>country_path(@country))
+      visit country_path(@country)
+      expect(page).to_not have_link('Add Destination', :href=>new_country_destination_path(@country))
+    end
+
+    it "does not display the 'Add Post' link" do
+      @country = FactoryGirl.create :country
+      visit country_path(@country)
+      expect(page).to_not have_link('Add Post', :href=>new_country_post_path(@country))
     end
   end
 
   context 'Navbar' do
      context 'when Travel Log logo is clicked' do
-       it 'displays the index page (root_path)' do
+       it 'displays the index page (root_path)', :type => :view do
          @country = FactoryGirl.create :country
          visit country_path @country
          click_on 'Travel Log'
-         expect(page).to have_css('h1', :text => 'Vacation Country Destinations')
+         expect(response).to render_template('countries/index')
        end
      end
   end
