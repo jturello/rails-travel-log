ddf657e86bf5a641e923be368721a9f8ebd050fb
James Turello <jturello@yahoo.com>
2017-02-06 13:48:43 -0800

    add country posts sorting tests [Finishes #139237497]

---
+++ spec/features/country_posts_list_sort_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+describe "country post list sorting with ajax" do
+  before :each do
+    @user_2 = create(:user, :username => 'User Two', :email => 'different@email.com')
+    @country = create(:country)
+    @post_1 = create(:country_post, :content => 'first post', :commentable => @country)
+    @post_2 = create(:country_post, :content => 'second post', :user => @user_2, :commentable => @country)
+  end
+  context "when user is not logged in" do
+    context "on initial Country Detail page load" do
+
+      it "displays post list in Last Updated DESC order" do
+        visit country_path(@post_1.commentable)
+        within(:xpath, "//table/tr[1]") do
+          expect(page).to have_content "second post"
+        end
+        within(:xpath, "//table/tr[2]") do
+          expect(page).to have_content("first post")
+        end
+      end
+    end
+
+    context "on clicking Last Updated header sort link" do
+
+      it "displays post list in Last Updated ASC order" do
+        visit country_path(@post_1.commentable)
+        click_on 'Last Updated'
+        within(:xpath, "//table/tr[1]") do
+          expect(page).to have_content "first post"
+        end
+        within(:xpath, "//table/tr[2]") do
+          expect(page).to have_content("second post")
+        end
+      end
+    end
+  end # End context when user not logged in
+
+  context "when user is logged in" do
+
+    before :each do
+      @user = create(:user, :username => "User Three", :email => 'another@email.com')
+      login_as(@user, :scope => :user)
+    end
+
+    context "on adding a new post via Ajax" do
+
+      it "prepends new post to top of post list", js: true  do
+        visit country_path(@post_1.commentable)
+        click_on 'Add Post'
+        fill_in 'Content', :with => 'Third post!'
+        click_on 'Create Post'
+        within(:xpath, "//tbody/tr[1]") do
+          expect(page).to have_content "Third post!"
+        end
+      end
+    end
+  end
+
+end
